3c3d68ca7b2de86d2b59f2300af6517d8e39ae65
davidshtif <davidshtif456@gmail.com>
2018-02-11 12:38:23 +0200

    first commit

---
+++ app/src/main/java/com/example/user/testfile_david/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.user.testfile_david;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et=(EditText)findViewById(R.id.editText);
+        tv=(TextView)findViewById(R.id.textView);
+    }
+
+    public void read(View view) {
+        String st=et.getText().toString();
+        FileOutputStream fos=null;
+        try {
+            fos=openFileOutput("text.txt",Context.MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        OutputStreamWriter osw=new OutputStreamWriter(fos);
+        BufferedWriter bw=new BufferedWriter(osw);
+        try {
+            bw.write(st);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write(View view) {
+        InputStream is=null;
+        try {
+            is=openFileInput("text.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader tmp=new InputStreamReader(is);
+        BufferedReader reader=new BufferedReader(tmp);
+        String str="";
+        StringBuffer buffer=new StringBuffer();
+        try {
+            while((str=reader.readLine())!=null){
+                buffer.append(str+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tv.setText(buffer);
+    }
+}
